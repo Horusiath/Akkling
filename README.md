e13be9e5b55a508a7b623102d95a0b6d4477e4de
Bartosz Sypytkowski <b.sypytkowski@gmail.com>
2015-12-17 22:00:40 +0100

    pre-release 2.1

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 This is the experimental fork of Akka.FSharp library, introducing new features such as typed actor refs, and also simplifying existing Akka.FSharp API. The main reason for splitting from official API is to be able to introduce new (also experimental), but possibly breaking changes outside existing Akka release cycle.
 
+**Read wiki pages for more info.**
+
 ## Hello world
 
 ```fsharp
---
+++ src/Akkling/Behaviors.fs
@@ -0,0 +1,31 @@
+﻿
+//-----------------------------------------------------------------------
+// <copyright file="Actors.fs" company="Akka.NET Project">
+//     Copyright (C) 2009-2015 Typesafe Inc. <http://www.typesafe.com>
+//     Copyright (C) 2013-2015 Akka.NET project <https://github.com/akkadotnet/akka.net>
+//     Copyright (C) 2015 Bartosz Sypytkowski <gttps://github.com/Horusiath>
+// </copyright>
+//-----------------------------------------------------------------------
+
+module Akkling.Behaviors
+
+/// <summary>
+/// An empty actor behavior, which ignores all incoming messages.
+/// </summary>
+let ignore (context:Actor<'Message>) : Behavior<'Message> = 
+    let rec loop () = actor {
+        let! _ = context.Receive()
+        return! loop ()
+    }
+    loop ()
+
+/// <summary>
+/// Actor behavior, which resends message back to it's sender.
+/// </summary>
+let echo (context: Actor<'Message>) : Behavior<'Message> =
+    let rec loop () = actor {
+        let! msg = context.Receive()
+        context.Sender () <! msg
+        return! loop ()    
+    }
+    loop ()
