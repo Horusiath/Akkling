eda7a2d64f16322bab73e0b6a1a4a9e585490b7a
Bartosz Sypytkowski <b.sypytkowski@gmail.com>
2016-02-25 13:23:09 +0100

    published v0.3.0

--- src/Akkling.Cluster.Sharding/AssemblyInfo.fs
+++ src/Akkling.Cluster.Sharding/AssemblyInfo.fs
@@ -1,41 +1,12 @@
-﻿namespace Akkling.Cluster.Sharding.AssemblyInfo
-
+﻿namespace System
 open System.Reflection
-open System.Runtime.CompilerServices
-open System.Runtime.InteropServices
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[<assembly: AssemblyTitle("Akkling.Cluster.Sharding")>]
-[<assembly: AssemblyDescription("")>]
-[<assembly: AssemblyConfiguration("")>]
-[<assembly: AssemblyCompany("")>]
-[<assembly: AssemblyProduct("Akkling.Cluster.Sharding")>]
-[<assembly: AssemblyCopyright("Copyright ©  2016")>]
-[<assembly: AssemblyTrademark("")>]
-[<assembly: AssemblyCulture("")>]
+[<assembly: AssemblyTitleAttribute("Akkling.Cluster.Sharding")>]
+[<assembly: AssemblyProductAttribute("Akkling")>]
+[<assembly: AssemblyDescriptionAttribute("F# wrapper library for Akka.NET")>]
+[<assembly: AssemblyVersionAttribute("0.3.0")>]
+[<assembly: AssemblyFileVersionAttribute("0.3.0")>]
+do ()
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[<assembly: ComVisible(false)>]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[<assembly: Guid("88387857-c31e-47f8-b377-11aa78d7ce71")>]
-
-// Version information for an assembly consists of the following four values:
-// 
-//       Major Version
-//       Minor Version 
-//       Build Number
-//       Revision
-// 
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [<assembly: AssemblyVersion("1.0.*")>]
-[<assembly: AssemblyVersion("1.0.0.0")>]
-[<assembly: AssemblyFileVersion("1.0.0.0")>]
-
-do
-    ()
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "0.3.0"
--- src/Akkling/AssemblyInfo.fs
+++ src/Akkling/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Akkling")>]
 [<assembly: AssemblyProductAttribute("Akkling")>]
 [<assembly: AssemblyDescriptionAttribute("F# wrapper library for Akka.NET")>]
-[<assembly: AssemblyVersionAttribute("0.2.1")>]
-[<assembly: AssemblyFileVersionAttribute("0.2.1")>]
+[<assembly: AssemblyVersionAttribute("0.3.0")>]
+[<assembly: AssemblyFileVersionAttribute("0.3.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.2.1"
+    let [<Literal>] Version = "0.3.0"
