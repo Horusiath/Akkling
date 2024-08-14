e679e07d3f43a497ef6317e1b762a4af7be10808
Vagif Abilov <vagif.abilov@nrk.no>
2024-08-14 22:38:53 +0200

    Added Directory.Build.props

---
+++ tests/Akkling.Tests/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitle("Akkling.Tests")>]
+[<assembly: AssemblyProduct("Akkling")>]
+[<assembly: AssemblyDescription("F# wrapper library for Akka.NET")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] AssemblyTitle = "Akkling.Tests"
+    let [<Literal>] AssemblyProduct = "Akkling"
+    let [<Literal>] AssemblyDescription = "F# wrapper library for Akka.NET"
