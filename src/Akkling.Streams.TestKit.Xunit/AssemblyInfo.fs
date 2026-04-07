a05c5d0f74d9c8fb3c0c3f6e8c2b8d703246416a
Vagif Abilov <vagif.abilov@nrk.no>
2026-04-07 19:56:24 +0200

    Added xunit.v3.

---
+++ src/Akkling.Streams.TestKit.Xunit/AssemblyInfo.fs
@@ -0,0 +1,18 @@
+﻿namespace System
+
+open System.Reflection
+
+[<assembly: AssemblyTitle("Akkling.Streams.TestKit")>]
+[<assembly: AssemblyProduct("Akkling")>]
+[<assembly: AssemblyDescription("F# wrapper library for Akka.NET")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    [<Literal>]
+    let AssemblyTitle = "Akkling.Streams.TestKit"
+
+    [<Literal>]
+    let AssemblyProduct = "Akkling"
+
+    [<Literal>]
+    let AssemblyDescription = "F# wrapper library for Akka.NET"
---
+++ src/Akkling.TestKit.Xunit/AssemblyInfo.fs
@@ -0,0 +1,18 @@
+﻿namespace System
+
+open System.Reflection
+
+[<assembly: AssemblyTitle("Akkling.TestKit")>]
+[<assembly: AssemblyProduct("Akkling")>]
+[<assembly: AssemblyDescription("F# wrapper library for Akka.NET")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    [<Literal>]
+    let AssemblyTitle = "Akkling.TestKit"
+
+    [<Literal>]
+    let AssemblyProduct = "Akkling"
+
+    [<Literal>]
+    let AssemblyDescription = "F# wrapper library for Akka.NET"
